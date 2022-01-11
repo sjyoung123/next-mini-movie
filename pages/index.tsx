94fb14f43506a450579f64ed2bd4ed17a3db0d2f
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-01-11 18:48:39 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,10 @@ const Home = ({ data: movies }: IMovies) => {
             className="movie"
             key={movie.id}
           >
-            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+            <img
+              alt="image"
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            />
             <h4>{movie.title}</h4>
           </div>
         ))}
